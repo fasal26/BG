fa007924702d4f26ba4014ea68b177bc8f1b96cb
fasal kp <fasalkp26@gmail.com>
2023-07-19 21:07:22 +0530

    Updates

---
+++ assets/YoutubeVideos-bb25e4cc.js
@@ -0,0 +1,9 @@
+import{_ as e,f as t,g as i,s as a}from"./index-2ceb8df4.js";const o={},s={class:"baba-flex-col baba-svc-container"},r=a(`<h2 class="baba-flex">Videos</h2><div class="youtube-list baba-flex"><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/-q1Xm7X7s_k" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/WNvuJPX9-h8" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/y9vh0MBotyI" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/HG7LTSqhq3w" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/EW7iS0i5Usg" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/Dv4e-UMHcDQ" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/82sZMmvJNjQ" loading="lazy">\r
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/chyHXnuzo8o" loading="lazy">\r
+            </iframe></div>`,2),h=[r];function c(m,d){return t(),i("div",s,h)}const n=e(o,[["render",c]]);export{n as default};
