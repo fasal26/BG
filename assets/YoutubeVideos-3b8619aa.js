61c3658b40836a2a06582e2de807cbda5ad56c42
fasal kp <fasalkp26@gmail.com>
2023-08-23 17:18:10 +0530

    Updates

--- assets/YoutubeVideos-3b8619aa.js
+++ assets/YoutubeVideos-3b8619aa.js
@@ -1,4 +1,4 @@
-import{_ as e,f as t,g as i,s as a}from"./index-8ae90062.js";const o={},s={class:"baba-flex-col baba-svc-container"},r=a(`<h2 class="baba-flex">Videos</h2><div class="youtube-list baba-flex"><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/-q1Xm7X7s_k" loading="lazy">\r
+import{_ as e,f as t,g as i,s as a}from"./index-0c6d5596.js";const o={},s={class:"baba-flex-col baba-svc-container"},r=a(`<h2 class="baba-flex">Videos</h2><div class="youtube-list baba-flex"><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/-q1Xm7X7s_k" loading="lazy">\r
             </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/WNvuJPX9-h8" loading="lazy">\r
             </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/y9vh0MBotyI" loading="lazy">\r
             </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/HG7LTSqhq3w" loading="lazy">\r
