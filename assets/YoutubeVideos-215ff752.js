e4be6b3efcdbbc0db00bacff6bf9064e8cc4a3ac
fasal <fasalkp26@gmail.com>
2023-10-30 20:01:41 +0530

    Updates

---
+++ assets/YoutubeVideos-215ff752.js
@@ -0,0 +1,9 @@
+import{_ as e,f as t,g as i,s as a}from"./index-de6fe6fc.js";const o={},s={class:"baba-flex-col baba-svc-container"},h=a(`<h2 class="baba-flex">Videos</h2><div class="youtube-list baba-flex"><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/-q1Xm7X7s_k" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/WNvuJPX9-h8" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/y9vh0MBotyI" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/HG7LTSqhq3w" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/EW7iS0i5Usg" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/Dv4e-UMHcDQ" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/82sZMmvJNjQ" loading="lazy">
+            </iframe><iframe width="100%" height="100%" src=" https://www.youtube.com/embed/chyHXnuzo8o" loading="lazy">
+            </iframe></div>`,2),c=[h];function m(r,d){return t(),i("div",s,c)}const n=e(o,[["render",m]]);export{n as default};
